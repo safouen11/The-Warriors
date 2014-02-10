af989b40324270588493aaaea8c3b6e75dcdb998
Naoufel <Naoufel@172.16.227.142>
2014-02-10 13:43:05 +0100

    New commit by ikbel

---
+++ Voyage--a la carte/src/voyage/a/la/carte/VoyageALaCarte.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package voyage.a.la.carte;
+
+/**
+ *
+ * @author Naoufel
+ */
+public class VoyageALaCarte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
