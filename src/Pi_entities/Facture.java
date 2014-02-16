ffd4f9f93ea437f861afcf0f0f79c2b8965c007d
Naoufel <Naoufel@172.16.227.142>
2014-02-16 23:27:35 +0100

    New commit by ikbel

---
+++ src/Pi_entities/Facture.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pi_entities;
+
+/**
+ *
+ * @author Naoufel
+ */
+public class Facture {
+    
+}
